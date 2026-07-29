79f610a0da94a865bb21e990aa2418bc763bdee6
Doina <doinyco@gmail.com>
2026-07-29 13:12:09 -0700

    Add components folder

---
+++ src/components/Progress.jsx
@@ -0,0 +1,10 @@
+
+const Progress = () => {
+  return (
+    <div className="progress">
+      <h2>Reading Progress</h2>
+    </div>
+  )
+}
+
+export default Progress
---
+++ src/components/ReadingList.jsx
@@ -0,0 +1,9 @@
+const ReadingList = () => {
+  return (
+    <div className="reading-list">
+      <h2>Reading List</h2>
+    </div>
+  )
+}
+
+export default ReadingList
